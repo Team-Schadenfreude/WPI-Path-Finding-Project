c8fb3c7d7987ce4bcfe36f5d1147b6a30b67685f
msmcconnell <msmcconnell@wpi.edu>
2015-11-14 18:01:08 -0500

    recommit

--- src/AStar/TestJunit.java
+++ src/AStar/TestJunit.java
@@ -38,7 +38,37 @@ public class TestJunit{
 	   System.out.println(path);
 	   assertEquals(1,path);
    }
-   
+   @Test
+   public void test_mcc_2(){
+	   Settings defaultSettings = new Settings(false,false,false);
+	   List<Node> map = new ArrayList<Node>();
+	   Node n1 = new Node(0,0, "A");
+	   Node n2 = new Node(1,0, "B");
+	   Node n3 = new Node(2,0, "C");
+	   Node n4 = new Node(3,0, "D");
+	   Node n5 = new Node(4,0, "E");
+	   Node n6 = new Node(5,0, "F");
+	   Node n7 = new Node(6,0, "G");
+	   n1.neighbors = new Edge[]{
+				new Edge(n3,AStar.getDistance(n1,n3)),
+		};
+	   n2.neighbors = new Edge[]{
+			   //new Edge(n1,AStar.getDistance(n1,n2)),
+				new Edge(n3,AStar.getDistance(n2,n3))
+		};
+	   n3.neighbors = new Edge[]{
+				new Edge(n2,AStar.getDistance(n2,n3)),
+				new Edge(n1,AStar.getDistance(n1,n3))
+		};
+	   map.add(n1);
+	   map.add(n2);
+	   map.add(n3);
+	   System.out.println(map);
+	   AStar astar = new AStar(map,defaultSettings);
+	   List<Node> path = astar.findPath(n1, n2);
+	   System.out.println(path);
+	   assertEquals(1,path);
+   }
    
 //   @Test(expected = ValueOutOfBoundsException.class)
 //   public void test10()throws ValueOutOfBoundsException{ //Test out of bounds execption where input is beyond maximum value
