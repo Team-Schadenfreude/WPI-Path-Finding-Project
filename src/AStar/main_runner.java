124730c4c1338af09bb578e0e5343cda6e7c6566
Thomas Lourenco <Thomas Lourenco@130.215.168.87>
2015-11-14 16:10:35 -0500

    Added default settings and methods to the main_runner class.

--- src/AStar/main_runner.java
+++ src/AStar/main_runner.java
@@ -1,8 +1,33 @@
 package AStar;
 
+import AStar.Settings;
+import AStar.Node;
+import java.util.List;
+
 public class main_runner {
 
+	Settings defaultSettings = new Settings(false, false, false);
+	
 	public static void main(String[] args){
 		
 	}
+	
+	//Method to find a node given a location.
+	public static Node findNodeAtLocation(String key)
+	{
+		return null;
+	}
+	
+	//Method to find the path given a start node and an end node.
+	public static List<Node> getPath(Node startNode, Node endNode)
+	{
+		return null;
+	}
+	
+	//Method to draw a path on a map, given said path.
+	public static void drawPath(List<Node> path)
+	{
+		
+	}
+	
 }
