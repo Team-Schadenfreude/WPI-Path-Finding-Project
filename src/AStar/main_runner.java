554974eafd12bd12f4a67024312f863a3e595367
msmcconnell <msmcconnell@wpi.edu>
2015-11-14 19:46:03 -0500

    Combination of Hash table and control

--- src/AStar/main_runner.java
+++ src/AStar/main_runner.java
@@ -3,15 +3,21 @@ package AStar;
 import AStar.Settings;
 import AStar.TestJunit;
 import AStar.Node;
+import AStar.AStar;
 import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
+import AStar.NodeList;
 
 public class main_runner {
 
-	Settings defaultSettings = new Settings(false, false, false);
+	private static Settings defaultSettings = new Settings(false, false, false);
+	private static NodeList nlist = new NodeList();
+	private static List<Node> map = new ArrayList<Node>();
 	
 	public static void main(String[] args){
 		Result result = JUnitCore.runClasses(TestJunit.class);
@@ -19,18 +25,30 @@ public class main_runner {
 	         System.out.println(failure.toString());
 	      }
 	      System.out.println(result.wasSuccessful());
-	}
-	
-	//Method to find a node given a location.
-	public static Node findNodeAtLocation(String key)
-	{
-		return null;
+	      
+	    Scanner reader = new Scanner(System.in);
+		System.out.println("Enter your start location: ");
+		String startLoc = reader.next();
+		System.out.println("Enter your destinationn: ");
+		String dest = reader.next();
+		reader.close();
+		
+		getPathFromString(startLoc, dest);
 	}
 	
 	//Method to find the path given a start node and an end node.
-	public static List<Node> getPath(Node startNode, Node endNode)
+	public static List<Node> getPathFromNode(Node startNode, Node endNode)
 	{
-		return null;
+		AStar astar = new AStar(map, defaultSettings);
+		return astar.findPath(startNode, endNode);
+	}
+	//Method to find path when given a string 
+	public static List<Node> getPathFromString(String startName, String destName)
+	{
+		Node startNode = nlist.findNode(startName);
+		Node destNode = nlist.findNode(destName);
+		return getPathFromNode(startNode, destNode);
+		//drawPath(path);
 	}
 	
 	//Method to draw a path on a map, given said path.
