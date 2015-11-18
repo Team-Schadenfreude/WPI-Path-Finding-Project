d460435e7f4321e8e59508ad9592ca93cc0d17c8
kewen <kewen@130.215.173.70>
2015-11-17 19:20:42 -0500

    add testfile

---
+++ test/AStar/TestAStar.java
@@ -0,0 +1,51 @@
+package AStar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+
+public class TestAStar extends TestCase{
+	/*
+	 * Tests for running AStar.
+	 */
+	
+	public void testConstructorOfAStarWithNoParameters(){
+		AStar astar = new AStar();
+		assertEquals(astar.getMap(), null);
+		assertEquals(astar.getSettings(), null);
+	}
+	
+	public void testConstructorOfAStarWithParameters(){
+		AStar astar = new AStar();
+		List<Node> map = new ArrayList<Node>();
+		Node n1 = new Node(0, 0, "A");
+		Node n2 = new Node(1, 0, "B");
+		Node n3 = new Node(2, 0, "C");
+		n1.neighbors = new ArrayList<>(Arrays.asList(new Edge(n3, AStar.getDistance(n1, n3))));
+		n2.neighbors = new ArrayList<>(Arrays.asList(new Edge(n3, AStar.getDistance(n2, n3))));
+		n3.neighbors = new ArrayList<>(Arrays.asList(new Edge(n2, AStar.getDistance(n2, n3)), new Edge(n1, AStar.getDistance(n1, n3))));
+		map.add(n1);
+		map.add(n2);
+		map.add(n3);
+		Settings defaultSettings = new Settings(false, false, false);
+		astar.setMap(map);
+		astar.setSettings(defaultSettings);
+		AStar astar_new = new AStar(map, defaultSettings);
+		assertEquals(astar.getMap(), astar_new.getMap());
+		assertEquals(astar.getSettings(), astar_new.getSettings());
+	}
+	
+	public void testBuildPath() {
+		
+	}
+	
+	public void testFindPath() {
+		
+	}
+
+
+	
+}
---
+++ test/GUI/TestGUI.java
@@ -0,0 +1,22 @@
+package GUI;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import AStar.Settings;
+import AStar.AStar;
+import AStar.Main;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class TestGUI {
+	/*
+	 * Tests for running GUI.
+	 */
+	public void testWindowInitialization() {
+		
+	}
+
+	
+}
