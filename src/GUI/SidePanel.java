3a105d9692d2e03ac28ccddcfa9f657e9abcdf37
kewen <kewen@130.215.221.229>
2015-12-09 19:00:02 -0500

    modified displaying array icons

--- src/GUI/SidePanel.java
+++ src/GUI/SidePanel.java
@@ -88,18 +88,6 @@ for(String s: directions) {
 				hbItem = new HBox(10, new ImageView(new Image("/res/icons/go_straight.png", iconSize, iconSize, true, true)), label);
 				hbList.add(hbItem);
 			}
-			else if(s.toLowerCase().contains("right turn")) {
-				label = new Label(s);
-				label.getStyleClass().add("direction_label");
-				hbItem = new HBox(10, new ImageView(new Image("/res/icons/turn_right.png", iconSize, iconSize, true, true)), label);
-				hbList.add(hbItem);
-			}
-			else if(s.toLowerCase().contains("left turn")) {
-				label = new Label(s);
-				label.getStyleClass().add("direction_label");
-				hbItem = new HBox(10, new ImageView(new Image("/res/icons/turn_left.png", iconSize, iconSize, true, true)), label);
-				hbList.add(hbItem);
-			}
 			else if(s.toLowerCase().contains("slight right turn")) {
 				label = new Label(s);
 				label.getStyleClass().add("direction_label");
@@ -122,6 +110,18 @@ for(String s: directions) {
 				label = new Label(s);
 				label.getStyleClass().add("direction_label");
 				hbItem = new HBox(10, new ImageView(new Image("/res/icons/turn_sharp_left.png", iconSize, iconSize, true, true)), label);
+				hbList.add(hbItem);
+			}
+			else if(s.toLowerCase().contains("right turn")) {
+				label = new Label(s);
+				label.getStyleClass().add("direction_label");
+				hbItem = new HBox(10, new ImageView(new Image("/res/icons/turn_right.png", iconSize, iconSize, true, true)), label);
+				hbList.add(hbItem);
+			}
+			else if(s.toLowerCase().contains("left turn")) {
+				label = new Label(s);
+				label.getStyleClass().add("direction_label");
+				hbItem = new HBox(10, new ImageView(new Image("/res/icons/turn_left.png", iconSize, iconSize, true, true)), label);
 				hbList.add(hbItem);
 			}
 			else if(s.toLowerCase().contains("proceed into")) {
