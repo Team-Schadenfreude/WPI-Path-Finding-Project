60199e10e0532fd6f26c9a9a1e7a77b370bcc40e
fschlic <fschlicht@wpi.edu>
2015-12-15 04:02:13 -0500

    Tests for the classes Room and Floor.

---
+++ test/DataAccess/TestFloor.java
@@ -0,0 +1,20 @@
+package DataAccess;
+
+import junit.framework.TestCase;
+
+public class TestFloor extends TestCase{
+//	Floor f;
+//	
+//	public void setUp(){
+//		f=new Floor("src/res/stratton_2.jpg", "Stratton");
+//	}
+//	
+//	public void testSetRooms(){
+//		
+//	}
+//	
+//	public void testGetRooms(){
+//		
+//	}
+
+}
