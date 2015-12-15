30b8182783dffbd4008e18cab13716a0f718c3ca
Jackson <Jackson@130.215.8.89>
2015-12-14 20:45:02 -0500

    Added an new icon for the program

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
  
@@ -21,6 +22,7 @@ public class Main extends Application {
     	FXMLLoader loader = new FXMLLoader();
     	loader.setLocation(Main.class.getResource("Main.fxml"));
         AnchorPane root = (AnchorPane) loader.load();
+        stage.getIcons().add(new Image("/res/icons/window_icon.png"));
         Scene scene = new Scene(root, root.getWidth(), root.getHeight());
         scene.getStylesheets().add("GUI/application.css");
         stage.setTitle("Randy Path Planner");
